07c59688f78d3a07b60d54c9cc8b64c990a2d5dc
cfs20 <132075071+cfs20@users.noreply.github.com>
2023-04-28 14:49:57 +0100

    Web-test1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function scrollFunction(nav) {
+  const yOffset = -$("nav").height() + 1;
+  const element = document.getElementById(nav);
+  const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+  window.scrollTo({ top: y, behavior: "smooth" });
+}
+
+const navScroll = document.querySelector("#nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    navScroll.classList.add("nav-scrolled");
+  } else if (window.scrollY <= 200) {
+    navScroll.classList.remove("nav-scrolled");
+  }
+});
