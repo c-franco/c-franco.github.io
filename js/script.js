75b466682b5abf6fb807e9e56ab15006537bdcb6
cfs20 <132075071+cfs20@users.noreply.github.com>
2023-05-02 18:43:01 +0100

    Web translate

--- js/script.js
+++ js/script.js
@@ -79,3 +79,69 @@ form.addEventListener("submit", function handleClick(event) {
   submitBtn.style.opacity = 0.65;
 });
 
+function ChangeLanguage() {
+  const flag = document.getElementById("change-lang-flag");
+  if (flag.src.includes("ES")) {
+    flag.src = "https://flagsapi.com/GB/flat/32.png";
+
+    $(".nav-about-me a").text("About me");
+    $(".nav-works a").text("Works");
+    $(".nav-contact a").text("Contact");
+    $("#job").text("Back-End Developer");
+    $(".about-me-title").text("About me");
+    $("#p1").html(
+      "Hi! My name is Christian Franco, I'm 21 years old and I'm a <span>back-end developer</span>."
+    );
+    $("#p2").html(
+      "My skills include handling programming languages such as <span>C#</span> and <span>Java</span>, " +
+        "as well as <span>database skills</span> and knowledge in web development. " +
+        "I currently have more than <span>a year of experience</span> in .NET software development."
+    );
+    $("#p3").html(
+      "Outside of work, I like to automate tasks through programming, develop my own mobile apps " +
+        "and even video games. Right now I am <span>looking for new projects</span>."
+    );
+    $("#works h1").text("Personal projects");
+    $("#contact h1").text("Contact");
+    $(".contact-message").text(
+      "If you have any questions or comments, don't hesitate to contact me!"
+    );
+    $(".form-name-label").text("Name");
+    $(".form-message-label").text("Message");
+    $("#input-submit").prop("value", "Send");
+    $(".mail-logo").text("Mail");
+  } else {
+    flag.src = "https://flagsapi.com/ES/flat/32.png";
+
+    $(".nav-about-me a").text("Sobre mí");
+    $(".nav-works a").text("Trabajos");
+    $(".nav-contact a").text("Contacto");
+    $("#job").text("Desarrollador Back-End");
+    $(".about-me-title").text("Sobre mí");
+    $("#p1").html(
+      "¡Hola! Mi nombre es Christian Franco, tengo 21 años y soy <span>desarrollador back-end</span>."
+    );
+    $("#p2").html(
+      "Mis habilidades incluyen el manejo de lenguajes de programación como " +
+        "<span>C#</span> y <span>Java</span>, al igual que competencias en " +
+        "<span>base de datos</span> y conocimientos en desarrollo web. " +
+        "Actualmente cuento con más de <span>un año de experiencia</span> en " +
+        "desarrollo de software en .NET."
+    );
+    $("#p3").html(
+      "Fuera del trabajo, me gusta automatizar tareas mediante la " +
+        "programación, desarrollar mis propias aplicaciones móviles e incluso " +
+        "videojuegos. Ahora mismo me encuentro en <span>búsqueda de nuevos proyectos</span>."
+    );
+    $("#works h1").text("Proyectos personales");
+    $("#contact h1").text("Contacto");
+    $(".contact-message").text(
+      "¿Tienes alguna pregunta o comentario? ¡No dudes en " +
+        "ponerte en contacto conmigo!"
+    );
+    $(".form-name-label").text("Nombre");
+    $(".form-message-label").text("Mensaje");
+    $("#input-submit").prop("value", "Enviar");
+    $(".mail-logo").text("Correo");
+  }
+}
